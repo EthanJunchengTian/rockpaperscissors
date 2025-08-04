2a881fc57c840a6cffc22847f1a9a9b6e27e5ba9
Ethan Tian <109113476+EthanJunchengTian@users.noreply.github.com>
2025-08-04 15:58:07 -0400

    please work gang

--- src.js
+++ src.js
@@ -30,3 +30,28 @@ function getComputerChoice () {
     } else return "scissors";
 }
 
+function playRound (player, computer) {
+    computer = getComputerChoice();
+    player = playerChoice;
+
+    if (
+            (computer === "rock" && player === "paper") ||
+            (computer === "paper" && player === "scissors") ||
+            (computer === "scissors" && player === "rock")
+        ) {
+            playerScore++;
+            resultBoard.innerHTML = `You Won!<br>Player choice: ${player}<br>Computer choice: ${computer}`;
+            scoreboardPlayer.textContent = playerScore;
+        } else if (
+            (computer === "rock" && player === "scissors") ||
+            (computer === "paper" && player === "rock") ||
+            (computer === "scissors" && player === "paper")
+        ) {
+            computerScore++;
+            resultBoard.innerHTML = `You Lost!<br>Player choice: ${player}<br>Computer choice: ${computer}`;
+            scoreboardComp.textContent = computerScore;
+        } else {
+            resultBoard.innerHTML = `It's A Draw!<br>Player choice: ${player}<br>Computer choice: ${computer}`;
+        }
+}
+
