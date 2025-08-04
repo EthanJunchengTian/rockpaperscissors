818994f7cda8048f26dbed98494b51abb03fb7bf
Ethan Tian <109113476+EthanJunchengTian@users.noreply.github.com>
2025-08-04 15:02:37 -0400

    be different

--- src.js
+++ src.js
@@ -2,3 +2,14 @@ const scoreboardPlayer = document.querySelector('.scoreboard-player')
 const scoreboardComp = document.querySelector('.scoreboard-computer')
 const finalscore = document.querySelector('.final-score')
 
+const buttons = document.querySelectorAll('button');
+
+let playerChoice = 0
+let playerChoiceNum = 0
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener
+})
